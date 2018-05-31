63f6ec05ab693f0542f12303f9ec57e8e169deab
Syoudai Yokoyama <>
2018-05-31 14:44:30 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Setup
+
+```sh
+$ /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+$ brew install ansible
+$ ansible-playbook -i hosts localhost.yml
+```
---
+++ roles/fish/templates/config.fish
@@ -0,0 +1,13 @@
+## rmでゴミ箱に入れる
+# alias rm='rmtrash'
+
+alias ec='/usr/local/bin/emacsclient'
+alias g='git'
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
+
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
